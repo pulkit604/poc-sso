73d0d2892ec7a82de425af1156b16e419b401036
pulkit604 <guppul@gmail.com>
2023-02-13 21:14:44 +0100

    add database related query, connect, disconnect functions

--- auth-provider/app.py
+++ auth-provider/app.py
@@ -7,3 +7,27 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 DATABASE = 'database/sso_manage.db'
+
+##Database related functions
+def get_db():
+    try:
+        db = getattr(g, '_database', None)
+        if db is None:
+            db = g._database = sqlite3.connect(DATABASE)
+        return db
+    except Exception as e:
+        print(e)
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+#Query helper function as stated in flask docs
+#https://flask.palletsprojects.com/en/2.2.x/patterns/sqlite3/
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv

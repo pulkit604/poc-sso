d50ea4020c78731237a3f43b1d5ea5b9ba207a0f
pulkit604 <guppul@gmail.com>
2023-02-13 21:08:57 +0100

    add success page

---
+++ src/views/Success.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>
+    Hello, You have successfully logged in using Pulkit's SSO!
+    <div style="width:100%"><div style="height:0;padding-bottom:56.25%;position:relative;width:100%"><iframe allowfullscreen="" frameBorder="0" height="100%" src="https://giphy.com/embed/8VDO7Fy2PFohfdAnpJ/video" style="left:0;position:absolute;top:0" width="100%"></iframe></div></div>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "Success"
+};
+</script>
+
+<style scoped>
+
+</style>

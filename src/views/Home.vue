20745b933820f66a00850b97ff9f5014a08c4d55
pulkit604 <guppul@gmail.com>
2023-02-13 21:08:35 +0100

    Update POC Homepage with a login button to sso

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h4>Hello, Welcome to this Proof-of-concept for testing Pulkit's SSO!</h4>
+    <a
+      :href="
+        authUri +
+          '?response_type=code&clientId=' +
+          clientId +
+          '&clientSecret=' +
+          clientSecret +
+          '&redirectUri=' +
+          redirectUri +
+          '&state=123yss'
+      "
+    ><button>Login using Pulkit's SSO</button></a>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      clientId: "Puoilj5i45",
+      clientSecret: "pouih4565h",
+      redirectUri: "http://localhost:8080/success",
+      authUri: "http://127.0.0.1:5000/auth",
+      scopes: []
+    }
   }
 };
 </script>
